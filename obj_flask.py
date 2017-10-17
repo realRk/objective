3b7d1c27cd9542a9779717b6725b46cf4b3e4a53
Rakesh R <rakeshramesh2000@gmail.com>
2017-10-17 17:40:41 +0530

    final changes are done using str() function

--- obj_flask.py
+++ obj_flask.py
@@ -14,76 +14,59 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    message = ""
-    message +="<h1>hello world</h1>"
-    message +="<input type = 'submit' value = 'list' action = '/list' >"
-    return message
-
-@app.route("/list")
-def list():
     rest = session.query(Restaurant).all()
-    messgae = ""
-    message = "<html><body>"
-    message += "<a href = '/restaurants/new'>Make new</a></br>"
-    for rst in rest:
-        message += rst.name
-        message += "</br>"
-        message += "<a href ='/Edit'>Edit</a>"
-        message += "</br>"
-        message += "<a href = #>Delete</a>"
-        message += "</br>"
-    message += "</body></html>"
-
-    return message
-
-@app.route("/listmenu")
-def listmenu():
-    rest = session.query(MenuItem).all()
-    messgae = ""
-    message = "<html><body>"
-    message += "<a href = '/restaurants/new'>Make new</a></br>"
-    for rst in rest:
-        message += rst.id
-        message += "</br>"
-        message += "<a href ='/Edit'>Edit</a>"
-        message += "</br>"
-        message += "<a href = #>Delete</a>"
-        message += "</br>"
-    message += "</body></html>"
-
-    return message
-
-
-
-@app.route("/details")
-def details():
-    rest = session.query(Restaurant).one()
     message = ""
-    message += "<html><body>"
-    message += rest.id
-    message +="</br>"
-    message +="</body></html>"
-
-    return message
-
-
-
-@app.route("/deta")
-def deta():
-    rest = session.query(Restaurant).first()
-    men = session.query(MenuItem).filter_by(id = rest.id)
-    message = ""
-    for mn in men:
-        message += men.name
+    for r in rest:
+        message += r.name
+        message += "</br>"
+        message += str(r.id)
         message += "</br>"
 
     return message
 
-@app.route("/check")
-def check():
 
-    restaurants = session.query(Restaurant).first()
-    return restaurants.id
+@app.route("/menuitem")
+def menuitem():
+    menu = session.query(MenuItem).all()
+    message = ""
+    for m in menu:
+        message += m.name
+        message += "</br>"
+        message += str(m.id)
+        message += "</br>"
+        message += str(m.price)
+        message += "</br>"
+        message += m.course
+        message += "</br>"
+        message += m.description
+        message += "</br>"
+        message += "</br></br>"
+
+    return message
+
+
+@app.route("/for_each")
+def for_each():
+    rest = session.query(Restaurant).all()
+    message = ""
+    for r in rest:
+        menu = session.query(MenuItem).filter_by(restaurant_id = r.id).all()
+        for m in menu:
+            message += "<h1>%s</h1>" % r.name
+            message += m.name
+            message += "</br>"
+            message += str(m.id)
+            message += "</br>"
+            message += str(m.price)
+            message += "</br>"
+            message += m.course
+            message += "</br>"
+            message += m.description
+            message += "</br>"
+            message += "</br></br>"
+
+    return message
+
 
 if __name__ == '__main__':
     app.debug = True
